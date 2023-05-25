30657bd21a7ce70a430d2632f50a39306083f6af
aisyahdlstr <aisyahdlestari11@gmail.com>
2023-05-25 18:41:01 +0700

    perhitungan

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 
 function hitungluas(){
-    var id = document.getElementById("id-sisi-luas").value;
-    console.log(id);
-    document.getElementById("output-luas").innerHTML = '<span>'+ id + '</span>';
+    var luas='';
+    var rumus = document.getElementById("rumus-luas").innerHTML = 'L = S X S';
+    var id = document.getElementById("sisi-luas").value;
 
-    var result= document.getElementsByClassName("result-luas").;
+    var perhitungan = document.getElementById("perhitungan-luas").innerHTML ='L =' + '<span>'+ id + '</span>'+ 'X'+'<span>'+ id + '</span>';
+    
+    luas = id * id;
+    document.getElementById("output-luas").innerHTML ='hasil = '+'<span>'+ luas+'</span>';
+}
+
+function hitungkeliling(){
+    var luas='';
+    var rumus = document.getElementById("rumus-keliling").innerHTML = 'K = 4 x S';
+    var id = document.getElementById("sisi-keliling").value;
+
+    var perhitungan = document.getElementById("perhitungan-keliling").innerHTML ='K =' + '<span>'+ 4 + '</span>'+ 'X'+'<span>'+ id + '</span>';
+    
+    luas = 4 * id;
+    document.getElementById("output-keliling").innerHTML ='hasil = '+'<span>'+ luas+'</span>';
+}
+
+function resetluas(){
+    document.getElementsByid(formluas).reset();
+}
+function resetkeliling(){
+    document.getElementsByid(formkeliling).reset();
 }
