64a60204aaf755cdabfb1a64a9f84eec6ed5f955
aisyahdlstr <aisyahdlestari11@gmail.com>
2023-05-25 18:53:18 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,24 +1,33 @@
 
 function hitungluas(){
     var luas='';
-    var rumus = document.getElementById("rumus-luas").innerHTML = 'L = S X S';
     var id = document.getElementById("sisi-luas").value;
+    if (id==""){
+        alert("Silahkan inputkan nilai")
+    } else {
+        luas = parseFloat(id) * parseFloat(id);
+        document.getElementById("rumus-luas").innerHTML = 'L = S X S';
+        var perhitungan = document.getElementById("perhitungan-luas").innerHTML ='L =' + '<span>'+ id + '</span>'+ 'X'+'<span>'+ id + '</span>';
+        document.getElementById("output-luas").innerHTML ='L = '+'<span>'+ luas+'</span>';
+    }
 
-    var perhitungan = document.getElementById("perhitungan-luas").innerHTML ='L =' + '<span>'+ id + '</span>'+ 'X'+'<span>'+ id + '</span>';
     
-    luas = id * id;
-    document.getElementById("output-luas").innerHTML ='hasil = '+'<span>'+ luas+'</span>';
+    // luas = id * id;
+    
 }
 
 function hitungkeliling(){
-    var luas='';
-    var rumus = document.getElementById("rumus-keliling").innerHTML = 'K = 4 x S';
+    var keliling='';
     var id = document.getElementById("sisi-keliling").value;
-
-    var perhitungan = document.getElementById("perhitungan-keliling").innerHTML ='K =' + '<span>'+ 4 + '</span>'+ 'X'+'<span>'+ id + '</span>';
+    if (id==""){
+        alert("Silahkan inputkan nilai")
+    } else{
+        keliling = 4 * parseFloat(id);
+        document.getElementById("rumus-keliling").innerHTML = 'K = 4 x S';
+        var perhitungan = document.getElementById("perhitungan-keliling").innerHTML ='K =' + '<span>'+ 4 + '</span>'+ 'X'+'<span>'+ id + '</span>';
+        document.getElementById("output-keliling").innerHTML ='K = '+'<span>'+ keliling+'</span>';
+    }
     
-    luas = 4 * id;
-    document.getElementById("output-keliling").innerHTML ='hasil = '+'<span>'+ luas+'</span>';
 }
 
 function resetluas(){
