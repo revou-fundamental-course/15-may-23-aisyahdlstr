cb382f510f679f20d9e9cbc1f182fa018d97e2d3
aisyahdlstr <aisyahdlestari11@gmail.com>
2023-05-25 15:52:19 +0700

    styling

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@
 function hitungluas(){
     var id = document.getElementById("id-sisi-luas").value;
     console.log(id);
+    document.getElementById("output-luas").innerHTML = '<span>'+ id + '</span>';
+
+    var result= document.getElementsByClassName("result-luas").;
 }
